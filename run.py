9937b9630c06e4b9a53536247219753ebf842a26
Ezequiel Torres <torreszeaks@gmail.com>
2024-07-29 22:51:26 -0700

    Adding run.py.

--- run.py
+++ run.py
@@ -1,26 +1,56 @@
 import boto3
+import subprocess
 from botocore.exceptions import ClientError
 
 
-# To get list of buckets present in AWS using S3 client
-def get_buckets_client():
-    session = boto3.session.Session()
-    # User can pass customized access key, secret_key and token as well
-    s3_client = session.client("s3")
-    response = s3_client.list_buckets()
-    buckets = []
-    for bucket in response["Buckets"]:
-        buckets += {bucket["Name"]}
-    return buckets
-
-
-def get_fileNames():
+def getPreviousSave():
     session = boto3.session.Session()
     # User can pass customized access key, secret_key and token as well
     s3_client = session.client("s3")
     response = s3_client.list_objects_v2(Bucket="vault-hunters")
-    for content in response["Contents"]:
-        s3_client.download_file("vault-hunters", content["Key"], "vault-hunters-new")
+    s3_client.download_file("world-new.zip", "vault-hunters", "world-new.zip")
+    process = subprocess.Popen(
+        "cp world-new.zip world-old.zip", shell=True, stdout=subprocess.PIPE
+    )
+    process.wait()
+
+    process = subprocess.Popen(
+        "echo 'eula=true' > eula.txt", shell=True, stdout=subprocess.PIPE
+    )
+    process.wait()
 
 
-print(get_fileNames())
+def uploadSave():
+    session = boto3.session.Session()
+    s3_client = session.client("s3")
+    process = subprocess.Popen("rm world-old.zip", shell=True, stdout=subprocess.PIPE)
+    process.wait()
+
+    process = subprocess.Popen(
+        "mv world-new.zip world-old.zip", shell=True, stdout=subprocess.PIPE
+    )
+    process.wait()
+
+    # Zip current world
+    process = subprocess.Popen(
+        "zip -r world-new.zip world", shell=True, stdout=subprocess.PIPE
+    )
+    process.wait()
+
+    # User can pass customized access key, secret_key and token as well
+    s3_client.upload_file("world-new.zip", "vault-hunters", "world-new.zip")
+    s3_client.upload_file("world-old.zip", "vault-hunters", "world-old.zip")
+
+
+def startServer():
+    process = subprocess.Popen(
+        "java -Xmx3072M -Xms3072M -jar server.jar nogui pause",
+        shell=True,
+        stdout=subprocess.PIPE,
+    )
+    process.wait()
+
+
+getPreviousSave()
+startServer()
+uploadSave()
