f4ef23110c35d534d6cc545021415eca9a2fea71
Ezequiel Torres <torreszeaks@gmail.com>
2024-07-29 19:07:53 -0700

    Adding boto3

--- run.py
+++ run.py
@@ -1,1 +1,26 @@
-print("Hello World")
+import Boto3
+
+
+class BucketWrapper:
+    """Encapsulates S3 bucket actions."""
+
+    def __init__(self, bucket):
+        """
+        :param bucket: A Boto3 Bucket resource. This is a high-level resource in Boto3
+                       that wraps bucket actions in a class-like structure.
+        """
+        self.bucket = bucket
+        self.name = bucket.name
+
+    @staticmethod
+    def list(s3_resource):
+        """
+        Get the buckets in all Regions for the current account.
+
+        :param s3_resource: A Boto3 S3 resource. This is a high-level resource in Boto3
+                            that contains collections and factory methods to create
+                            other high-level S3 sub-resources.
+        :return: The list of buckets.
+        """
+        buckets = list(s3_resource.buckets.all())
+        print("Got buckets: %s.", buckets)
