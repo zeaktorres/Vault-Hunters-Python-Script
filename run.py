e8241cf8c12b344386cf7b856ad59ddd9f3532ad
Ezequiel Torres <torreszeaks@gmail.com>
2024-07-29 20:42:41 -0700

    Updating run.py.

--- run.py
+++ run.py
@@ -1,14 +1,26 @@
 import boto3
 from botocore.exceptions import ClientError
 
+
 # To get list of buckets present in AWS using S3 client
 def get_buckets_client():
     session = boto3.session.Session()
     # User can pass customized access key, secret_key and token as well
-    s3_client = session.client('s3')
+    s3_client = session.client("s3")
     response = s3_client.list_buckets()
-    buckets =[]
-    for bucket in response['Buckets']:
-      buckets += {bucket["Name"]}
+    buckets = []
+    for bucket in response["Buckets"]:
+        buckets += {bucket["Name"]}
+    return buckets
 
-print(get_buckets_client())
+
+def get_fileNames():
+    session = boto3.session.Session()
+    # User can pass customized access key, secret_key and token as well
+    s3_client = session.client("s3")
+    response = s3_client.list_objects_v2(Bucket="vault-hunters")
+    for content in response["Contents"]:
+        s3_client.download_file("vault-hunters", content["Key"], "vault-hunters-new")
+
+
+print(get_fileNames())
