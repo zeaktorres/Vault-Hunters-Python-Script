144f8a35c22a054f9950bc1585b717da4e6eedad
Ezequiel Torres <torreszeaks@gmail.com>
2024-07-29 19:33:22 -0700

    Fixing syntax.

--- run.py
+++ run.py
@@ -3,18 +3,12 @@ from botocore.exceptions import ClientError
 
 # To get list of buckets present in AWS using S3 client
 def get_buckets_client():
-   session = boto3.session.Session()
-   # User can pass customized access key, secret_key and token as well
-   s3_client = session.client('s3')
-   try:
-      response = s3_client.list_buckets()
-      buckets =[]
-   for bucket in response['Buckets']
+    session = boto3.session.Session()
+    # User can pass customized access key, secret_key and token as well
+    s3_client = session.client('s3')
+    response = s3_client.list_buckets()
+    buckets =[]
+    for bucket in response['Buckets']:
       buckets += {bucket["Name"]}
 
-      except ClientError:
-         print("Couldn't get buckets.")
-         raise
-      else:
-         return buckets
 print(get_buckets_client())
